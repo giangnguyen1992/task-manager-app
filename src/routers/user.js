3a67af6352d1ffbdfec88939c8333593da3de833
giangnguyen1992 <tgn1992@hotmail.de>
2021-10-15 20:37:42 +0200

    adding auth to all user routes

--- src/routers/user.js
+++ src/routers/user.js
@@ -56,7 +56,7 @@ router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
 
-router.get("/users/:id", async (req, res) => {
+/* router.get("/users/:id", async (req, res) => {
   try {
     const _id = req.params.id;
     const user = await User.findById(_id);
@@ -64,9 +64,9 @@ router.get("/users/:id", async (req, res) => {
   } catch (e) {
     res.status(500).send();
   }
-});
+}); */
 
-router.patch("/users/:id", async (req, res) => {
+router.patch("/users/me", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["name", "email", "password", "age"];
   const isValidOperation = updates.every((update) =>
@@ -78,32 +78,25 @@ router.patch("/users/:id", async (req, res) => {
   }
 
   try {
-    const _id = req.params.id;
-    const user = await User.findById(_id);
-    updates.forEach((update) => (user[update] = req.body[update]));
-    await user.save();
-    /* const user = await User.findByIdAndUpdate(_id, req.body, {
-      new: true,
-      runValidators: true,
-    }); */
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
 
-    if (!user) return res.status(404).send;
-
-    res.send(user);
+    res.send(req.user);
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-router.delete("/users/:id", async (req, res) => {
+router.delete("/users/me", auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
+    const user = await User.findByIdAndDelete(req.user._id);
 
-    if (!user) {
+    /* if (!user) {
       return res.status(404).send();
-    }
+    } */
 
-    res.send(user);
+    await req.user.remove();
+    res.send(req.user);
   } catch (e) {
     res.status(500).send();
   }
