6b834b9114d5a940b9ea865c86f8c5636b1a88ed
giangnguyen1992 <tgn1992@hotmail.de>
2021-10-18 22:53:17 +0200

    added env

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api");
+  await mongoose.connect(process.env.MONGODB_URL);
 }
 
 main().catch((err) => console.log(err));
