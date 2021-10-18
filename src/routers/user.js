66cdf2324acb32f16396fcd383203540edcfaba5
giangnguyen1992 <tgn1992@hotmail.de>
2021-10-18 21:54:41 +0200

    added file upload support

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,46 +1,66 @@
 const express = require("express");
+const multer = require("multer");
 
 const router = new express.Router();
 
 const auth = require("../middleware/auth");
 const User = require("../models/user");
 
-router.post("/users", async (req, res) => {
-  try {
-    const user = new User(req.body);
-    const newUser = await user.save();
-    const token = await user.generateAuthToken();
+router.post(
+  "/users",
+  async (req, res) => {
+    try {
+      const user = new User(req.body);
+      const newUser = await user.save();
+      const token = await user.generateAuthToken();
 
-    res.status(201).send({ user: newUser, token });
-  } catch (e) {
-    res.status(400).send(e);
+      res.status(201).send({ user: newUser, token });
+    } catch (e) {
+      res.status(400).send(e);
+    }
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
   }
-});
+);
 
-router.post("/users/login", async (req, res) => {
-  try {
-    const user = await User.findByCredentials(
-      req.body.email,
-      req.body.password
-    );
-    const token = await user.generateAuthToken();
-    res.send({ user, token });
-  } catch (e) {
-    res.status(400).send();
+router.post(
+  "/users/login",
+  async (req, res) => {
+    try {
+      const user = await User.findByCredentials(
+        req.body.email,
+        req.body.password
+      );
+      const token = await user.generateAuthToken();
+      res.send({ user, token });
+    } catch (e) {
+      res.status(400).send();
+    }
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
   }
-});
+);
 
-router.post("/users/logout", auth, async (req, res) => {
-  try {
-    req.user.token = req.user.tokens.filter((token) => {
-      return token.token !== req.token;
-    });
-    await req.user.save();
-    res.send();
-  } catch (e) {
-    res.status(500).send();
+router.post(
+  "/users/logout",
+  auth,
+  async (req, res) => {
+    try {
+      req.user.token = req.user.tokens.filter((token) => {
+        return token.token !== req.token;
+      });
+      await req.user.save();
+      res.send();
+    } catch (e) {
+      res.status(500).send();
+    }
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
   }
-});
+);
 
 router.post("/users/logoutall", auth, async (req, res) => {
   try {
@@ -52,9 +72,16 @@ router.post("/users/logoutall", auth, async (req, res) => {
   }
 });
 
-router.get("/users/me", auth, async (req, res) => {
-  res.send(req.user);
-});
+router.get(
+  "/users/me",
+  auth,
+  async (req, res) => {
+    res.send(req.user);
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
 /* router.get("/users/:id", async (req, res) => {
   try {
@@ -66,39 +93,108 @@ router.get("/users/me", auth, async (req, res) => {
   }
 }); */
 
-router.patch("/users/me", auth, async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ["name", "email", "password", "age"];
-  const isValidOperation = updates.every((update) =>
-    allowedUpdates.includes(update)
-  );
+router.patch(
+  "/users/me",
+  auth,
+  async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["name", "email", "password", "age"];
+    const isValidOperation = updates.every((update) =>
+      allowedUpdates.includes(update)
+    );
 
-  if (!isValidOperation) {
-    return res.status(400).send({ error: "invalid operation" });
+    if (!isValidOperation) {
+      return res.status(400).send({ error: "invalid operation" });
+    }
+
+    try {
+      updates.forEach((update) => (req.user[update] = req.body[update]));
+      await req.user.save();
+
+      res.send(req.user);
+    } catch (e) {
+      res.status(400).send(e);
+    }
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
   }
+);
 
-  try {
-    updates.forEach((update) => (req.user[update] = req.body[update]));
-    await req.user.save();
+router.delete(
+  "/users/me",
+  auth,
+  async (req, res) => {
+    try {
+      const user = await User.findByIdAndDelete(req.user._id);
 
-    res.send(req.user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-router.delete("/users/me", auth, async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.user._id);
-
-    /* if (!user) {
+      /* if (!user) {
       return res.status(404).send();
     } */
 
-    await req.user.remove();
-    res.send(req.user);
+      await req.user.remove();
+      res.send(req.user);
+    } catch (e) {
+      res.status(500).send();
+    }
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+const upload = multer({
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("Please upload a correct image file"));
+    }
+
+    cb(undefined, true);
+  },
+});
+
+router.post(
+  "/users/me/avatar",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
+    res.status(200).send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+router.delete(
+  "/users/me/avatar",
+  auth,
+  async (req, res) => {
+    req.user.avatar = undefined;
+    await res.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+router.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user || user.avatar) {
+      throw new Error();
+    }
+
+    res.set("Content-Type", "image/jpg");
+    res.send(user.avatar);
   } catch (e) {
-    res.status(500).send();
+    res.status(404).send();
   }
 });
 
